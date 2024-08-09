6b5450f13b2f130fe0fb7f0ed0fc9712560a6c50
Max Pfeiffer <max@maxpfeiffer.ch>
2024-08-09 22:32:05 +0200

    Simplyfied container test

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,9 +1,9 @@
 """Basic test fixtures."""
 
 import pytest
-from python_on_whales import Builder, Container, DockerClient, Image
+from python_on_whales import Builder, DockerClient, Image
 
-from tests.constants import EXPOSED_CONTAINER_PORT, PLATFORM, TEST_IMAGE_TAG
+from tests.constants import PLATFORM, TEST_IMAGE_TAG
 from tests.utils import get_docker_context, get_dockerfile
 
 
@@ -51,27 +51,6 @@ def docker_image(docker_client: DockerClient, builder: Builder) -> Image:
     yield image
 
     image.remove(force=True)
-
-
-@pytest.fixture(scope="function")
-def container(docker_client: DockerClient, docker_image: Image):
-    """Run the Docker image.
-
-    :param docker_client:
-    :param docker_image:
-    :return:
-    """
-    dyn_dns_container: Container = docker_client.container.run(
-        docker_image,
-        platform=PLATFORM,
-        publish=[(EXPOSED_CONTAINER_PORT, "8080")],
-        detach=True,
-    )
-
-    yield dyn_dns_container
-
-    dyn_dns_container.stop()
-    dyn_dns_container.remove()
 
 
 @pytest.fixture(scope="function")
