01b1da9cd7826e074d1a5e81f3402546b6ec0916
Max Pfeiffer <max@maxpfeiffer.ch>
2024-08-18 18:07:57 +0200

    Finalised permissions and secrets for Opentofu

--- opentofu/locals.tf
+++ opentofu/locals.tf
@@ -5,4 +5,7 @@ locals {
   docker_file = "${local.repo_root}/container/Dockerfile"
   pyproject_toml = "${local.repo_root}/pyproject.toml"
   environment_variables = { for tuple in regexall("(.*)=(.*)", file(".env")) : tuple[0] => tuple[1] }
+  secrets = { for tuple in regexall("(.*)=(.*)", file(".secrets")) : tuple[0] => sensitive(tuple[1]) }
+  route53_policy = "${abspath(path.root)}/route53_policy.json"
+  secrets_manager_policy = "${abspath(path.root)}/secrets_manager_policy.json"
 }
--- opentofu/main.tf
+++ opentofu/main.tf
@@ -40,6 +40,15 @@ provider "docker" {
   }
 }
 
+resource "aws_secretsmanager_secret" "token" {
+  name = local.secrets["CLIENT_ID"]
+}
+
+resource "aws_secretsmanager_secret_version" "token" {
+  secret_id     = aws_secretsmanager_secret.token.id
+  secret_string = local.secrets["TOKEN"]
+}
+
 module "docker_image" {
 	source = "terraform-aws-modules/lambda/aws//modules/docker-build"
 
@@ -55,16 +64,26 @@ module "docker_image" {
 module "lambda_function" {
   source = "terraform-aws-modules/lambda/aws"
 
+  # Name and description
   function_name = local.aws_name
   description   = "Simple Dynamic DNS"
 
+  # Function config
   create_package = false
-
   image_uri    = module.docker_image.image_uri
   package_type = "Image"
   architectures = ["arm64"]
+  timeout = 5
 
+  # Function URL
   create_lambda_function_url = true
+  authorization_type = "NONE"
 
+  # Environment variables
   environment_variables = local.environment_variables
+
+  # Policies
+  attach_policy_jsons = true
+  policy_jsons = [file(local.secrets_manager_policy), file(local.route53_policy)]
+  number_of_policy_jsons = 2
 }
